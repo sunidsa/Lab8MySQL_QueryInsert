f389914f326916b43384d76d56a361aff20aaf5d
SirapraphaCY <siraprapha_ch@kkumail.com>
2020-10-05 22:43:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab8mysql_queryinsert/ViewHolder.kt
@@ -0,0 +1,12 @@
+package com.example.lab8mysql_queryinsert
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.std_item_layout.view.*
+
+class ViewHolder (view: View):RecyclerView.ViewHolder(view){
+    val tvID : TextView = view.tv_id
+    val tvName : TextView = view.tv_name
+    val tvAge : TextView = view.tv_age
+}
